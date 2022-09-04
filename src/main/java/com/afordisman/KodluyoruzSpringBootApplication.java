e8cc06da51c3e0bf1df85c4fca32562748a1e306
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-04 22:02:11 +0300

    Initial commit

---
+++ src/main/java/com/afordisman/KodluyoruzSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.afordisman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
+    }
+
+}
