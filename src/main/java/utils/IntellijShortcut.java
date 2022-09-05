4ec48a0d40d968e96481f1faffa7d36f1ae2e1cf
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-05 23:07:39 +0300

    SpringBootApplication ==> Security (exclude)

---
+++ src/main/java/utils/IntellijShortcut.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class IntellijShortcut {
+/*    Setting ==> ctrl+alt+s
+    Alt+Ins ==> constructor, toString, getter setter
+    Ctrl+/ => single comment
+    Ctrl+shift+/ => multiple comment*/
+}
