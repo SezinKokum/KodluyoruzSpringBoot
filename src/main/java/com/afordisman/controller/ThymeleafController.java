64059f272e89c9d46bb2892c0b0956ee8805f895
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-10 17:45:49 +0300

    @PathVariable, name, required

--- src/main/java/com/afordisman/controller/ThymeleafController.java
+++ src/main/java/com/afordisman/controller/ThymeleafController.java
@@ -4,6 +4,7 @@ import com.afordisman.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -12,6 +13,11 @@ import java.util.List;
 @Controller
 public class ThymeleafController {
 
+    //optional
+    @GetMapping({"/","index"})
+    public String index(){
+        return "index";
+    }
     //Ctrl+alt+L formatter
     //http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
@@ -84,5 +90,19 @@ public class ThymeleafController {
 
         model.addAttribute("product_liste", listem);
         return "thymeleaf6"; //direkt bu isimdeki thymeleaf.html sayfasına yönlendirdi
+    }
+
+    //@PathVariable
+    //http://localhost:8080/thymeleaf7/
+    //http://localhost:8080/thymeleaf7/4
+    @GetMapping({"/thymeleaf7","/thymeleaf7/{id}"})
+    //@ResponseBody //herhangi bir sayfaya yönlendirmeyip direkt gösteriyor
+    public String getThymeleaf7ModelObject(Model model, @PathVariable(name = "id", required=false) Long id) {
+       if(id!=null){
+           model.addAttribute("key_model1", "id"+ id );
+       }else{
+           model.addAttribute("key_model1", "id bulunamadı");
+       }
+        return "thymeleaf7"; //direkt bu isimdeki thymeleaf.html sayfasına yönlendirdi
     }
 }
