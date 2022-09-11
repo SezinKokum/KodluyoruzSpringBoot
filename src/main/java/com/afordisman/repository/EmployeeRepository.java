d416b743af918897208179a2b03a495ebc3e8c16
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-11 22:16:41 +0300

    @Repository (Jpa Hibernate)
    extends JpaRepository
    extends CrudRepository

---
+++ src/main/java/com/afordisman/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.afordisman.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository>CrudRepository
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+
+
+}
