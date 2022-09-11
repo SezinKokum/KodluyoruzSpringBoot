faf045b880c312e8fe86bd39d78f89e9f49ee8ea
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-12 00:11:40 +0300

    EmployeeServices

---
+++ src/main/java/com/afordisman/business/services/EmployeeServices.java
@@ -0,0 +1,22 @@
+package com.afordisman.business.services;
+
+import com.afordisman.business.dto.EmployeeDto;
+import com.afordisman.data.entity.EmployeeEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmployeeServices {
+
+    //CRUD
+    public List<EmployeeDto> getAllEmployees();
+    public EmployeeDto createEmployee(EmployeeDto employeeDto);
+    public ResponseEntity<EmployeeDto> getEmployeeById(Long id) throws Throwable;
+    public ResponseEntity<EmployeeDto> updateEmployee(Long id, EmployeeDto employeeDto) throws Throwable;
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(Long id) throws Throwable;
+
+    //Model Mapper
+    public EmployeeDto EntityToDto(EmployeeEntity employeeEntity);
+    public EmployeeEntity DtoToEntity(EmployeeDto employeeDto);
+}
