124104da4f50449bd8c43c18825291a9c5d24653
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-13 23:58:05 +0300

    PASSWORD ENCODER

--- src/main/java/com/afordisman/bean/PasswordEncoderBean.java
+++ src/main/java/com/afordisman/bean/PasswordEncoderBean.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;*/
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 //@Configuration
 public class PasswordEncoderBean {
 
- //   @Bean
-   /* public PasswordEncoder passwordEncoderBean(){
+    @Bean
+    public PasswordEncoder passwordEncoderBean(){
         return new BCryptPasswordEncoder();
-    }*/
+    }
 }
